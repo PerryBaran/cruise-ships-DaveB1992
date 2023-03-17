848f03758c9c8765bd53e5cf98083360e8de386f
Dave Bratby <dbratby1992@gmail.com>
2023-03-17 14:14:57 +0000

    added a starting port and created the test to checck for it

--- __test__/ship.test.js
+++ __test__/ship.test.js
@@ -1,7 +1,11 @@
-const ship = require('../src/ship.js')
+const Ship = require('../src/ship.js')
 
 describe('constructor', () => {
     it('returns an object', () => {
-        expect( new ship('ship')).toBeInstanceOf(Object)
+        expect(new Ship('Ship')).toBeInstanceOf(Object)
     })
+   it('has a starting port', () => {
+        const myShip = new Ship('Dover')
+        expect(myShip.startingPort).toBe('Dover')
+   })
 })
--- src/ship.js
+++ src/ship.js
@@ -2,12 +2,16 @@
 As a cruise ship captain,
 So I can get passengers aboard a ship,
 I want a ship to have a starting port.
+
+Object: Ship
+method:
 */
 const port = 'Dover'
 
-function ship() {
+function Ship() {
     this.startingPort = port
-
 }
 
-module.exports = ship
+
+
+module.exports = Ship

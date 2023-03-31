1a60b47c49bfaa052a95178a4840a25339551682
Dave Bratby <dbratby1992@gmail.com>
2023-03-31 13:26:53 +0100

    updated port to have the remove ship test pass

--- __test__/port.test.js
+++ __test__/port.test.js
@@ -1,45 +1,39 @@
 const Port = require('../src/port.js')
 
 describe('port constructor', () => {
-    describe('has ports, can add and remove ships', () => {
-        let myPort
+    describe('has ports and ships', () => {
         let port
-        let ship 
         let theBlackPearl
-        let theFlyingDutchmen
+        let theFlyingDutchman
         
         beforeEach(() => {
-            myPort = new Port ('Amsterdam')
             port = new Port ('Amsterdam')
             ship = jest.fn()
             theBlackPearl = jest.fn()
-            theFlyingDutchmen = jest.fn()
+            theFlyingDutchman = jest.fn()
         })
         it('returns an object', () => {
             
-            expect(new Port('Port')).toBeInstanceOf(Object)
+            expect(new Port()).toBeInstanceOf(Object)
         })
         it('has a name property', () => {
-            const myPort = new Port('Amsterdam')
     
-            expect(myPort.name).toBe('Amsterdam')
+            expect(port.name).toBe('Amsterdam')
         })
         it('can add a ship', () => {
-            const port = new Port ('Amsterdam')
-            const ship = jest.fn()
     
             port.addShip(ship)
     
             expect(port.ships).toContain(ship)
         })
-        it('can remove a ship', () =>{
+        it('can remove a ship', () => {
+
             const port = new Port('Amsterdam')
-            const theBlackPearl = jest.fn()
-            const theFlyingDutchmen = jest.fn()
-    
+            const theBlackPearl = {}
+            const theFlyingDutchman = {}
+
             port.addShip(theBlackPearl)
-            port.addShip(theFlyingDutchmen)
-    
+            port.addShip(theFlyingDutchman)
             port.removeShip(theBlackPearl)
     
             expect(port.ships).toEqual([theBlackPearl])
